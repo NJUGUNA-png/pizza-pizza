9101ffa426336d4e425cab2010c41bcbcd9af84a
Njuguna <engbrian01@gmail.com>
2025-06-15 18:53:28 +0300

    added pizza model with ingredients and restaurant relationships

---
+++ server/models/pizza.py
@@ -0,0 +1,21 @@
+from server.app import db
+
+class Pizza(db.Model):
+    __tablename__ = 'pizzas'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    ingredients = db.Column(db.String(500), nullable=False)
+    
+    
+    restaurant_pizzas = db.relationship('RestaurantPizza', back_populates='pizza', cascade='all, delete-orphan')
+    
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'ingredients': self.ingredients
+        }
+    
+    def __repr__(self):
+        return f'<Pizza {self.name}>'
